04addf37b6dc4255e4bf15c32f33becfe1cc0a16
Michael Washer <mwasher@redhat.com>
2022-09-22 17:22:16 +1000

    Adding scripts as executable

--- etcd/2. Encrypted/perform_query.sh
+++ etcd/2. Encrypted/perform_query.sh
@@ -1,8 +1,18 @@
 # /bin/bash
+set -x
+source ./env_vars.sh
 
 # Communicate with the ETCD using certificates
 ETCDCTL_API=3 etcdctl --endpoints 127.0.0.1:2379 \
    --cert=${ETCD_SERVER_CERT} \
    --key=${ETCD_SERVER_KEY} \
    --cacert=${ETCD_CA} \
+   --write-out=table \
+   endpoint health
+
+ETCDCTL_API=3 etcdctl --endpoints 127.0.0.1:2379 \
+   --cert=${ETCD_SERVER_CERT} \
+   --key=${ETCD_SERVER_KEY} \
+   --cacert=${ETCD_CA} \
+   --write-out=table \
    member list
