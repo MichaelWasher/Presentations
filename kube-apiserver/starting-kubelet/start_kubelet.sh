fe14ac03fc972f07b2d8d2dbe224b9fc2778e0c0
Michael Washer <mwasher@redhat.com>
2022-10-12 14:13:08 +1000

    stashing

---
+++ kube-apiserver/starting-kubelet/start_kubelet.sh
@@ -0,0 +1,21 @@
+
+
+    kubelet \
+      --config=/etc/kubernetes/kubelet.conf \
+      --bootstrap-kubeconfig=/etc/kubernetes/kubeconfig \
+      --kubeconfig=/var/lib/kubelet/kubeconfig \
+      --container-runtime=remote \
+      --container-runtime-endpoint=/var/run/crio/crio.sock \
+      --runtime-cgroups=/system.slice/crio.service \
+      --node-labels=node-role.kubernetes.io/master,node.openshift.io/os_id=${ID} \
+      --node-ip=${KUBELET_NODE_IP} \
+      --minimum-container-ttl-duration=6m0s \
+      --cloud-provider= \
+      --volume-plugin-dir=/etc/kubernetes/kubelet-plugins/volume/exec \
+       \
+      --hostname-override=${KUBELET_NODE_NAME} \
+      --register-with-taints=node-role.kubernetes.io/master=:NoSchedule \
+      --pod-infra-container-image=quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:8b75cbb25d16e48afd464eeaa6676ec6a181a1b5db455c57f5f466bf4c7bd319 \
+      --system-reserved=cpu=${SYSTEM_RESERVED_CPU},memory=${SYSTEM_RESERVED_MEMORY} \
+      --v=${KUBELET_LOG_LEVEL}
+
