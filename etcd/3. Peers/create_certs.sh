be08123162a6fee875da8a615f4e0cebd7b692b7
Michael Washer <mwasher@redhat.com>
2024-02-01 21:26:09 +1000

    Untested

--- etcd/3. Peers/create_certs.sh
+++ etcd/3. Peers/create_certs.sh
@@ -5,18 +5,18 @@ source ./env_vars.sh
 echo "Configuring Certificates"
 
 # Create CAcert to be shared for all communication
-openssl req -nodes -x509 -newkey rsa:2048  -config san.cnf  -keyout ${ETCD_CA}.key -out ${ETCD_CA}.crt -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=root/emailAddress=sample@sample.com"
+openssl req -nodes -x509 -newkey rsa:2048  -config san.cnf  -keyout ${ETCD_CLIENT_CA}.key -out ${ETCD_CLIENT_CA}.crt -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=client-ca/emailAddress=sample@sample.com"
+openssl req -nodes -x509 -newkey rsa:2048  -config san.cnf  -keyout ${ETCD_PEER_CA}.key -out ${ETCD_PEER_CA}.crt -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=peer-ca/emailAddress=sample@sample.com"
 
 # Generate CSR and Sign.
-### NOTE: >> All ETCD Certificates are signed by the same CA <<
-# ETCD0
-openssl req -nodes -newkey rsa:2048  -config san.cnf  -keyout "${ETCD0_SERVER}.key" -out "${ETCD0_SERVER}.csr" -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=localhost/emailAddress=sample@sample.com"
-openssl x509 -req  -in "${ETCD0_SERVER}.csr" -CA "${ETCD_CA}.crt" -CAkey "${ETCD_CA}.key" -CAcreateserial -out "${ETCD0_SERVER}.crt" -extfile ./san.cnf -extensions v3_req
+# Create Client Serving Certificates
+for CRT in ${ETCD0_CLIENT_CERT_SERV} ${ETCD1_CLIENT_CERT_SERV} ${ETCD2_CLIENT_CERT_SERV} ; do
+    openssl req -nodes -newkey rsa:2048  -config san.cnf  -keyout "${CRT}.key" -out "${CRT}.csr" -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=localhost/emailAddress=sample@sample.com"
+    openssl x509 -req  -in "${CRT}.csr" -CA "${ETCD_CLIENT_CA}.crt" -CAkey "${ETCD_CLIENT_CA}.key" -CAcreateserial -out "${CRT}.crt" -extfile ./san.cnf -extensions v3_req
+done
 
-# ETCD1
-openssl req -nodes -newkey rsa:2048  -config san.cnf  -keyout "${ETCD1_SERVER}.key" -out "${ETCD1_SERVER}.csr" -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=localhost/emailAddress=sample@sample.com"
-openssl x509 -req  -in "${ETCD1_SERVER}.csr" -CA "${ETCD_CA}.crt" -CAkey "${ETCD_CA}.key" -CAcreateserial -out "${ETCD1_SERVER}.crt" -extfile ./san.cnf -extensions v3_req
-
-# ETCD2
-openssl req -nodes -newkey rsa:2048  -config san.cnf  -keyout "${ETCD2_SERVER}.key" -out "${ETCD2_SERVER}.csr" -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=localhost/emailAddress=sample@sample.com"
-openssl x509 -req  -in "${ETCD2_SERVER}.csr" -CA "${ETCD_CA}.crt" -CAkey "${ETCD_CA}.key" -CAcreateserial -out "${ETCD2_SERVER}.crt" -extfile ./san.cnf -extensions v3_req
+# Create Peer Serving and Auth Certificates
+for CRT in ${ETCD0_PEER_CERT_SERV} ${ETCD1_PEER_CERT_SERV} ${ETCD2_PEER_CERT_SERV}
+    openssl req -nodes -newkey rsa:2048  -config san.cnf  -keyout "${CRT}.key" -out "${CRT}.csr" -subj "/C=AU/ST=QLD/L=Brisbane/O=RedHat/OU=root/CN=localhost/emailAddress=sample@sample.com"
+    openssl x509 -req  -in "${CRT}.csr" -CA "${ETCD_PEER_CA}.crt" -CAkey "${ETCD_PEER_CA}.key" -CAcreateserial -out "${CRT}.crt" -extfile ./san.cnf -extensions v3_req
+done
--- etcd/3. Peers/env_vars.sh
+++ etcd/3. Peers/env_vars.sh
@@ -2,10 +2,16 @@
 # This script is used as a configuration point to provide settings without having to make changes to the other scripts.
 
 # Security / Certificate Environments
-export PKI="./certs"
-export CA=${PKI}/ca.crt
-export ETCD_CA=${PKI}/ca
 
-export ETCD0_SERVER=${PKI}/etcd0/server
-export ETCD1_SERVER=${PKI}/etcd1/server
-export ETCD2_SERVER=${PKI}/etcd2/server
+export ETCD_CLIENT_CA=client-ca
+export ETCD_PEER_CA=peer-ca
+
+# Client Serving Certificates
+export ETCD0_CLIENT_CERT_SERV=etcd-0/client-serving
+export ETCD1_CLIENT_CERT_SERV=etcd-1/client-serving
+export ETCD2_CLIENT_CERT_SERV=etcd-2/client-serving
+
+# PEER Certificates
+export ETCD0_PEER_CERT_SERV=etcd-0/peer-serving
+export ETCD1_PEER_CERT_SERV=etcd-1/peer-serving
+export ETCD2_PEER_CERT_SERV=etcd-2/peer-serving
